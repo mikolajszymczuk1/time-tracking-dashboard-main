9763cf87f136a334b45edfb6b4ff05137ab4f242
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-11-01 22:53:35 +0100

    Create getters, mutations and actions to manage data

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,36 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import data from "@/data/data.json";
+Vue.use(Vuex);
 
-Vue.use(Vuex)
+const state = {
+    timeData: data,
+    currentTimeframe: "weekly"
+}
+
+export const getters = {
+    toLowerCaseVersion: () => (text) => text.replace(" ", "-").toLowerCase(),
+    getCategories: (state, getters) => state.timeData.map(el => getters.toLowerCaseVersion(el.title)),
+    getSingleCategory: (state, getters) => (categoryName) => {
+        return state.timeData.filter(el => getters.toLowerCaseVersion(el.title) === categoryName)[0];
+    }
+}
+
+export const mutations = {
+    changeTimeframe(state, newTimeframe) {
+        state.currentTimeframe = newTimeframe;
+    }
+}
+
+export const actions = {
+    changeTimeframe(context, newTimeframe) {
+        context.commit("changeTimeframe", newTimeframe);
+    }
+}
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state,
+    getters,
+    mutations,
+    actions
+});
